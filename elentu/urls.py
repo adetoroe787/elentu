8b13f1256dc93063609f5ea5fc25bf1f6cc9931d
Ezekiel Adetoro <adetoroe787@gmail.com>
2022-02-04 21:32:20 +0100

    first commit

---
+++ elentu/urls.py
@@ -0,0 +1,19 @@
+from django.contrib import admin
+from django.urls import path,include
+from django.contrib.auth import views as auth_views
+from main import views as user_views
+from django.contrib.auth.decorators import login_required
+from django.conf import settings
+from django.conf.urls.static import static
+#from django.conf.urls import include, path
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include('core.urls')),
+]
+
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+
